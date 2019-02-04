75ae1d03ef1379bb11b05902ede5dc8c42814059
Kaloyan Nikolov <kaloyan.nikolov@telerik.com>
2019-02-04 22:59:20 +0200

    Initial commit

---
+++ track-metric.js
@@ -0,0 +1,18 @@
+const appInsights = require("applicationinsights");
+
+module.exports = function(RED) {
+    function TrackMetric(config) {
+        
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        appInsights.setup(config.iKey).start();
+        var appInsightsClient = appInsights.defaultClient;
+        
+        node.on('input', function(msg) {
+            appInsightsClient.trackMetric(msg.payload);
+            node.send(msg);
+        });
+    }
+    RED.nodes.registerType("track-metric", TrackMetric);
+}
